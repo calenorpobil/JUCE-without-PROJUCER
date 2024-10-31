b6f2f4e56b726161542c3c8e3ba37faed5d1822a
Marco Postman <89901767+calenorpobil@users.noreply.github.com>
2024-10-31 10:03:57 +0100

    Functional building

---
+++ test/source/AudioProcessorTest.cpp
@@ -0,0 +1,9 @@
+#include <YourPluginName/PluginProcessor.h>
+#include <gtest/gtest.h>
+
+namespace audio_plugin_test {
+TEST(AudioPluginAudioProcessor, Foo) {
+	AudioPluginAudioProcessor processor{};
+	ASSERT_FALSE(true);
+}
+}
